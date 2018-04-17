b3b5b037be3da698260914f10cd6c4568c6abc15
thananan <thananan157@hotmail.com>
2018-04-17 16:39:45 +0700

    Hello Test water 91%

--- app/src/test/java/workshop/testing/data/model/RecipeTest.java
+++ app/src/test/java/workshop/testing/data/model/RecipeTest.java
@@ -21,8 +21,36 @@ public class RecipeTest {
         assertEquals("Water",recipe.title);
         assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
 
+    }
 
+    @Test
+    public void i_can_read_data_from_mixed(){
+        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+
+        Recipe recipe = Recipe.readFromStream(inputStream);
+
+        assertNotNull(recipe);
+        assertEquals("punch",recipe.id);
+        assertEquals("Punch",recipe.title);
+        assertEquals("Juice of 3 lemons\n" +
+                "1 orange\n" +
+                "1 pint grape juice\n" +
+                "1 cup sugar\n" +
+                "1 cup water\n" +
+                "1 pine apple juice\n" +
+                "Mix all together and strain. Add large piece of ice.", recipe.description);
 
     }
+
+    @Test
+    public void exception_test(){
+        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/no_id.txt");
+
+        Recipe recipe = Recipe.readFromStream(inputStream);
+
+        assertNull(recipe);
+
+    }
+
 
 }
